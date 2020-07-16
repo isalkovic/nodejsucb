eac6cd279e28cab40aaff094539b681bb6915f91
Ivo Salkovic <ivo.salkovic@gmail.com>
2020-07-16 10:28:54 +0200

    kkk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from  Ivo! 888- this is time with registry keys!!-ok--!!!");
+  res.send("Hello from  Ivo! 888- this is time with registry hhhkeys!!-ok--!!!");
 });
  
 module.exports.app = app;
